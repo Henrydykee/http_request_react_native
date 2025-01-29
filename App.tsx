5705a7c49bf26f9247996ec54f6bdf691b4dc7d4
ugochukwu <dykeehenry@gmail.com>
2025-01-29 17:57:26 +0100

    Add Manage Expense screen and update navigation structure

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ import AllExpenseScreen from './screens/all_expense_screen';
 import RecentExpenseScreen from './screens/recent_expense_screen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import ManageExpenseScreen from './screens/manage_expense_screen';
+import { GlobalStyles } from './utils/style';
+import  { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -13,35 +17,53 @@ export default function App() {
 
   function MyTabs() {
     return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={RecentExpenseScreen} />
-        <Tab.Screen name="Profile" component={AllExpenseScreen} />
+      <Tab.Navigator screenOptions={{
+        headerStyle : {backgroundColor : GlobalStyles.colors.primary500 },
+        headerTintColor : 'white',
+        headerTitleStyle : { fontWeight : 'bold' },
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500  },
+        // tabBarLabelStyle : { fontSize: 10, },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}>
+        <Tab.Screen name="Recent Expense" component={RecentExpenseScreen}
+        options={{
+          title: 'Recent Expense',
+          tabBarLabel: 'Recent Expense',
+          tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color} />
+        }}
+        
+        />
+        <Tab.Screen name="All Expense" component={AllExpenseScreen} 
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All Expenses',
+          tabBarIcon: ({color, size}) => <Ionicons name='list' size={size} color={color} />
+        }}
+        />
+
       </Tab.Navigator>
     )
   }
 
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-      screenOptions={
-        {
-          headerShown: false
-        }
-      }
-       >
-        <Stack.Screen name="Main" component={MyTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Screen name='Manage Expense' component={ManageExpenseScreen} />
+        <Stack.Navigator
+          screenOptions={
+            {
+              headerShown: false
+            }
+          }
+        >
+          <Stack.Screen name="Main" component={MyTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+
 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
