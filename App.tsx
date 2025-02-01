5500ff7a69cd4a0b86336c66c072d2709c941e97
ugochukwu <dykeehenry@gmail.com>
2025-02-01 13:17:13 +0100

    Update StatusBar style to inverted for improved visibility

--- App.tsx
+++ App.tsx
@@ -62,7 +62,7 @@ export default function App() {
 
   return (
     <>
-    <StatusBar style="auto" />
+    <StatusBar style="inverted" />
       <ExpenseContextProvider>
       <NavigationContainer>
         <Stack.Navigator
